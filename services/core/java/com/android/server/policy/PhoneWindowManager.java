600c5839d675d36de499e85cb007c88cbe6a43c2
minaripenguin <minaripenguin@users.noreply.github.com>
2024-12-06 09:21:22 +0100

    services: Introduce Shake Gestures Co-authored-by: AmeChanRain <downloadbot007@gmail.com> Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -266,6 +266,8 @@ import lineageos.providers.LineageSettings;
 import org.lineageos.internal.buttons.LineageButtons;
 import org.lineageos.internal.util.ActionUtils;
 
+import org.blackiron.server.ShakeGestureService;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -681,6 +683,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mSwapCapacitiveKeys = false;
     ANBIHandler mANBIHandler;
     private boolean mANBIEnabled;
+
+    private ShakeGestureService mShakeGestures;
 
     // Tracks user-customisable behavior for certain key events
     private Action mBackLongPressAction;
@@ -7114,6 +7118,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         "Three Fingers Swipe");
             }
         });
+        
+        mShakeGestures = ShakeGestureService.getInstance(mContext);
+        mShakeGestures.setScreenshotCallback(new ShakeGestureService.ScreenshotCallback() {
+            @Override
+            public void onScreenshotTaken() {
+                interceptScreenshotChord(TAKE_SCREENSHOT_FULLSCREEN, SCREENSHOT_KEY_OTHER, 0 /*pressDelay*/);
+            }
+        });
+        mShakeGestures.onStart();
 
         mLineageHardware = LineageHardwareManager.getInstance(mContext);
 
